56de9ce4f79d18c18ece9eb79c38a17d1e8466f2
Alvaro Diez Rey <alvarodiezrey@MacBook-Pro-de-Alvaro.local>
2023-04-09 23:44:36 +0200

    projects

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -4,7 +4,7 @@ interface ProjectCard {
   title: string;
   image: string;
   technologies: string;
-  link: string;
+  link?: string;
   github: string;
   description: string;
 }
@@ -15,10 +15,14 @@ export const ProjectCard = ({title, image, technologies, link, github, descripti
       <div className="flex flex-col text-center">
         <img src={image} alt={title} className="object-contain" />
         <h3 className="font-bold text-xl mt-2">{title}</h3>
-        <span className="italic text-sm mb-4 font-medium">{technologies}</span>
+        <span className="italic text-sm mb-4 mt-1 font-medium">{technologies}</span>
         <p className="mb-4 px-3">{description}</p>
         <div className="flex gap-x-3 self-center">
-          <button className="bg-[--main-color] px-5 py-2 rounded-3xl text-[#fafafa] font-bold border border-[--main-color] hover:text-[--main-color] hover:bg-[#fafafa]">Visit</button>
+          {
+            link
+              ? <a href={link} target="_blank" className="bg-[--main-color] px-5 py-2 rounded-3xl text-[#fafafa] font-bold border border-[--main-color] hover:text-[--main-color] hover:bg-[#fafafa] cursor-pointer">Visit</a>
+              : null
+          }
           <a href={github} target="_blank">
             <img src={githubIcon} alt="Code" className='w-10 h-10 hover:opacity-80 hover:cursor-pointer' />
           </a>
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,4 +1,5 @@
 import { ProjectCard } from '../components/ProjectCard';
+import portfolio from '../assets/portfolio.png';
 import metaversus from '../assets/metaversus.png';
 import evogym from '../assets/evogym.png';
 import colorgame from '../assets/colorgame.png';
@@ -8,35 +9,34 @@ export const Projects = () => {
   const projects = [
     {
       title: 'Portfolio',
-      image: metaversus,
-      technologies: "React, TypeScript, Tailwind",
-      link: "",
+      image: portfolio,
+      technologies: "React | TypeScript | Tailwind | Formik | Yup",
       github: "https://github.com/alvarodr97/portfolio-2023",
       description: "This portfolio website. You can check the code below."
     },
     {
       title: 'Metaversus',
       image: metaversus,
-      technologies: "Next.js, motion, Tailwind",
-      link: "",
-      github: "",
-      description: ""
+      technologies: "Next.js | motion | Tailwind",
+      link: "https://nextjs-metaversus-alvarodr97.vercel.app/",
+      github: "https://github.com/alvarodr97/nextjs-metaversus",
+      description: "Mockup website just to test some responsive designs."
     },
     {
       title: 'Evogym',
       image: evogym,
-      technologies: "React, TypeScript, motion, Tailwind",
-      link: "",
-      github: "",
-      description: ""
+      technologies: "React | TypeScript | motion | Tailwind",
+      link: "https://evogym-alvarodr97.vercel.app/",
+      github: "https://github.com/alvarodr97/evogym-react",
+      description: "Mockup website just to test some responsive designs."
     },
     {
       title: 'Colorgame',
       image: colorgame,
-      technologies: "React, TypeScript, Tailwind",
+      technologies: "React | TypeScript | Tailwind",
       link: "https://alvarodr97-colorgame.netlify.app/",
-      github: "",
-      description: ""
+      github: "https://github.com/alvarodr97/colorgame",
+      description: "A little minigame to test dark mode with tailwind."
     }
   ]
 
@@ -44,15 +44,14 @@ export const Projects = () => {
     <section id="projects">
       <div className="section-div pt-24">
         <h2 className="section-div-h2 mb-6">Projects</h2>
-        <div className='grid items-center w-full grid-cols-1 grid-rows-5 gap-6 px-5 mb-16 sm:px-10 sm:grid-cols-2 sm:grid-rows-3 lg:grid-cols-3 lg:grid-rows-2 sm:mt-0'>
+        <div className='grid items-center w-full grid-cols-1 grid-rows-4 gap-6 px-5 mb-16 sm:px-10 sm:grid-cols-2 sm:grid-rows-2 lg:grid-cols-3 lg:grid-rows-2'>
           {
             projects.map(project => (
-              <ProjectCard {...project} />
+              <ProjectCard key={project.title} {...project} />
             ))
           }
         </div>
       </div>
-
     </section>
   )
 }
